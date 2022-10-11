c6cd5eef4a79138da17191211f19ada2718eaa9f
Enzo Falvo <enzo.fvo@gmail.com>
2022-10-11 20:11:48 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+# Enzo Falvo
+
+# a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 1000 palavras.
+from bs4 import BeautifulSoup
+import requests
+
+sentenca = ''
+lista_sentencas = []
+
+
+url = "https://coldplay.fandom.com/wiki/Coldplay"
+
+
+informacoes_pagina = requests.get(url)
+html = BeautifulSoup(informacoes_pagina.text, 'html.parser')
+
+# Enzo Falvo
+# b) O texto desta página deverá ser transformado em um array de senteças.
+contador = 1
+
+for sentenca in html.find_all("p"):
+    lista_sentencas.append(sentenca.get_text())
+    print('Sentença número', contador, ':', sentenca.get_text())
+    contador += 1
+
+# Print de todo o array de sentenças
+print('Lista de sentenças:', lista_sentencas)
